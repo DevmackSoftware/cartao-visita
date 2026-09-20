221b16309bd1acf6ee04f29e683d589890f54d0f
nelio <info@devmack.com>
2026-09-20 08:22:57 +0200

    update angelo

--- src/app/angelo-ngovene/page.tsx
+++ src/app/angelo-ngovene/page.tsx
@@ -5,18 +5,18 @@ import { FcBullish, FcHome, FcPackage } from "react-icons/fc";
 export default function AngeloNgovenePage() {
     return (
         <main className="bg-white flex-col p-4 text-black w-full min-h-screen flex justify-center items-center">
-            <div className="flex flex-col flex-1 space-y-4 max-w-[375px] w-full relative md:h-auto">
+            <div className="flex flex-col flex-1 space-y-4 max-w-93.75 w-full relative md:h-auto">
                 <div className="border-2 aspect-video relative mb-20
                    before:absolute before:border-t-2 before:top-1 before:left-1 before:w-10 before:h-10 before:border-l-2
                    after:absolute after:border-b-2 after:bottom-1 after:right-1 after:w-10 after:h-10 after:border-r-2
                 ">
-                    <img
+                    {/* <img
                         className="object-cover aspect-video"
                         src="./profile.JPG"
                         alt="Profile image"
 
-                    />
-                    <div className="shadow-xl shadow-black/50 border-2  w-3/4 absolute left-4 -bottom-1/3 bg-white/50 backdrop-blur-2xl p-4
+                    /> */}
+                    <div data-aos="flip-left" className="shadow-xl shadow-black/50 border-2  w-3/4 absolute left-4 -bottom-1/3 bg-white/50 backdrop-blur-2xl p-4
                     before:absolute before:border-t-2 before:top-1 before:left-1 before:w-5 before:h-5 before:border-l-2
                     after:absolute after:border-b-2 after:bottom-1 after:right-1 after:w-5 after:h-5 after:border-r-2
                     ">
@@ -28,6 +28,7 @@ export default function AngeloNgovenePage() {
                 <div className="grid grid-cols-2 gap-2 w-full">
 
                     <a href="#"
+                        data-aos="flip-right"
                         className="bg-black flex-col text-white justify-center items-center flex p-4 relative font-semibold
                                         before:absolute before:border-t-2 before:top-1 before:left-1 before:w-5 before:h-5 before:border-l-2
                                         after:absolute after:border-b-2 after:bottom-1 after:left-1 after:w-5 after:h-5 after:border-l-2
@@ -38,6 +39,7 @@ export default function AngeloNgovenePage() {
                         <span className="text-zinc-500 text-xs">&#91;+258 84 000 000&#93;</span>
                     </a>
                     <a href="#"
+                        data-aos="flip-left"
                         className="bg-black text-white justify-center items-center flex p-4 relative font-semibold flex-col
                                         before:absolute before:border-t-2 before:top-1 before:right-1 before:w-5 before:h-5 before:border-r-2
                                         after:absolute after:border-b-2 after:bottom-1 after:right-1 after:w-5 after:h-5 after:border-r-2
@@ -45,7 +47,7 @@ export default function AngeloNgovenePage() {
                         <span>Guardar</span>
                         <span className="text-zinc-500 text-xs">&#91;+258 84 000 000&#93;</span>
                     </a>
-                    <a href="" className="flex-col border-2 p-4 col-span-2 flex justify-center items-center font-semibold"
+                    <a data-aos="flip-up" href="#" className="flex-col border-2 p-4 col-span-2 flex justify-center items-center font-semibold"
 
                     >
                         <span>Enviar email</span>
@@ -53,7 +55,7 @@ export default function AngeloNgovenePage() {
                     </a>
 
                 </div>
-                <div className="flex-1 *:grid *:place-content-center *:bg-black divide-x divide-y divide-zinc-500 text-white grid grid-cols-2">
+                <div data-aos="flip-down" className="flex-1 *:grid *:place-content-center *:bg-black divide-x divide-y divide-zinc-500 text-white grid grid-cols-2">
                     <a href="#" className="p-4 relative space-y-1
                     before:absolute before:border-t-2 before:top-1 before:left-1 before:w-5 before:h-5 before:border-l-2
                     ">
